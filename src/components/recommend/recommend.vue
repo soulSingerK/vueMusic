36ba4e4ca562a386a59ba67089e7008a424b6745
soulSingerK <425139963@qq.com>
2017-08-16 22:41:48 +0800

    modify

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div class="slider-wrapper" v-if="recommends.length">
-        <slider>
-          <div v-for="item in recommends">
-            <a :href="item.linkUrl">
-              <img class="class" :src="item.picUrl"></img>
-            </a>
-          </div>
-        </slider>
+    <scroll class="recommend-content" :data="discList">
+      <div>
+        <div class="slider-wrapper" v-if="recommends.length">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img class="class" :src="item.picUrl"></img>
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="item in discList">
+              <div class="icon">
+                <img :src="item.imgurl" height="60" width="60" />
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll'
 import {getRecommend, getDiscList} from 'api/getRecommend'
 import {ERR_OK} from 'api/config'
 import slider from 'base/slider/slider'
@@ -40,13 +57,14 @@ export default {
     _getDiscList() {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          this.discList = res.list
+          this.discList = res.data.list
         }
       })
     }
   },
   components: {
-    slider
+    slider,
+    Scroll
   }
 }
 </script>
